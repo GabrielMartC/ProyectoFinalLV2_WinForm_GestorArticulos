2a0df5407f96a112862b0c1ea4da67d38073a54f
GabrielM <gabrielmsistemas@gmail.com>
2024-05-27 23:36:42 -0300

    Primera subida. Separacion en capas: presentacion, dominio, negocio

---
+++ dominio/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }// para hacer las relaciones entre tablas
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion {  get; set; }
+        public Marca MarcaArticulo { get; set; }
+        public Categoria CategoriaArticulo { get; set; }
+        public string UrlImagen { get; set; }
+        public double Precio { get; set; } //fijarse el equivalente en tipo money
+
+        public Articulo() { }
+
+
+    }
+
+}
---
+++ presentacion/frmPrincipal.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace presentacion
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stastrEstado = new System.Windows.Forms.StatusStrip();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnLista = new System.Windows.Forms.Button();
+            this.pbImagenArticulo = new System.Windows.Forms.PictureBox();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenArticulo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stastrEstado
+            // 
+            this.stastrEstado.Location = new System.Drawing.Point(0, 405);
+            this.stastrEstado.Name = "stastrEstado";
+            this.stastrEstado.Size = new System.Drawing.Size(784, 22);
+            this.stastrEstado.TabIndex = 0;
+            this.stastrEstado.Text = "statusStrip1";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(0, 303);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(154, 94);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(0, 203);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(154, 94);
+            this.btnModificar.TabIndex = 2;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(0, 103);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(154, 94);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnLista
+            // 
+            this.btnLista.Location = new System.Drawing.Point(0, 3);
+            this.btnLista.Name = "btnLista";
+            this.btnLista.Size = new System.Drawing.Size(154, 94);
+            this.btnLista.TabIndex = 4;
+            this.btnLista.Text = "Listar";
+            this.btnLista.UseVisualStyleBackColor = true;
+            // 
+            // pbImagenArticulo
+            // 
+            this.pbImagenArticulo.Location = new System.Drawing.Point(551, 5);
+            this.pbImagenArticulo.Name = "pbImagenArticulo";
+            this.pbImagenArticulo.Size = new System.Drawing.Size(221, 281);
+            this.pbImagenArticulo.TabIndex = 5;
+            this.pbImagenArticulo.TabStop = false;
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(161, 3);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(384, 394);
+            this.dgvArticulos.TabIndex = 6;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 427);
+            this.Controls.Add(this.dgvArticulos);
+            this.Controls.Add(this.pbImagenArticulo);
+            this.Controls.Add(this.btnLista);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.stastrEstado);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenArticulo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip stastrEstado;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnLista;
+        private System.Windows.Forms.PictureBox pbImagenArticulo;
+        private System.Windows.Forms.DataGridView dgvArticulos;
+    }
+}
+
